d21dd9aeb5e7cef1b6ba7d172a6dbe3396da8be4
elcue <kimelcue@gmail.com>
2016-01-29 17:05:29 +0900

    파일 업로드 1번 까지. 올라가진않음.

--- src/main/java/first/sample/controller/SampleController.java
+++ src/main/java/first/sample/controller/SampleController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -58,10 +59,10 @@ public class SampleController {
 	
 	//글쓰기
 	@RequestMapping(value="/sample/insertBoard.do")
-	public ModelAndView insertBoard(CommandMap commandMap) throws Exception{
+	public ModelAndView insertBoard(CommandMap commandMap, HttpServletRequest request) throws Exception{
 	    ModelAndView mv = new ModelAndView("redirect:/sample/openBoardList.do");
 	     
-	    sampleService.insertBoard(commandMap.getMap());
+	    sampleService.insertBoard(commandMap.getMap(), request);
 	     
 	    return mv;
 	}
--- src/main/java/first/sample/service/SampleService.java
+++ src/main/java/first/sample/service/SampleService.java
@@ -3,12 +3,14 @@ package first.sample.service;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface SampleService {
 
 	List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception;
 
 	//글 쓰기
-	void insertBoard(Map<String, Object> map) throws Exception;
+	void insertBoard(Map<String, Object> map, HttpServletRequest request) throws Exception;
 	
 	//글 읽기 & 조회수증가
 	Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception;
